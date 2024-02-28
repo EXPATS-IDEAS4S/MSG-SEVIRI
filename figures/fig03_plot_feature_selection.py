a3e9abb1ed3dfbe33e88f3a69aef77b756124c85
Clau Clouds <37664450+ClauClouds@users.noreply.github.com>
2024-02-28 18:50:45 +0100

    code to select feature combinations

---
+++ figures/fig03_plot_feature_selection.py
@@ -0,0 +1,25 @@
+"""
+code to plot feature distributions and combine selections
+of feature values of identifying precipitation and convection
+"""
+
+
+from readers.files_dirs import path_dir_tree, path_figs
+
+
+def main():
+    
+    print('boh')
+
+
+
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
+        
